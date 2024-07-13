bc6b156be9db82633560a9d787eb3305ec9eea54
Nikki Odd <archiektor@gmail.com>
2024-07-13 17:45:51 +0200

    - added basic routes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import {Stack} from 'expo-router';
+import React, {useEffect} from 'react';
+import {SplashScreen, Stack} from 'expo-router';
+import {useFonts} from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 // In the context of the expo-router, the Slot component is used as a placeholder for nested routes.
 // The Slot component acts similarly to the Outlet component in other routing libraries like
 // react-router-dom. It allows you to define a layout that can render child routes within it.
 
 const RootLayout = () => {
+    const [fontsLoaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded && !error) {
+        return null;
+    }
+
     return (
         <Stack>
             <Stack.Screen name={'index'} options={{headerShown: false}}/>
